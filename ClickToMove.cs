67fd1145718ea8ec304ae54ad90dbaeb7aa53069
Sondre Edvardsen <sondreedvardsen@gmail.com>
2015-07-10 18:25:56 +0200

    First
    first

---
+++ ClickToMove.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToMove : MonoBehaviour 
+{
+	public float speed;
+	public CharacterController controller;
+	private Vector3 position;
+
+	public AnimationClip run;
+	public AnimationClip idle;
+
+	public static bool attack;
+	public static bool die;
+
+	public static Vector3 cursorPosition;
+
+	public static bool busy;
+
+	// Use this for initialization
+	void Start () 
+	{
+		position = transform.position;
+		busy = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(!busy)
+		{
+			locateCursor();
+			if(!attack&&!die)
+			{
+				if(Input.GetMouseButton(0))
+				{
+					//Locate where the player clicked on the terrain
+					locatePosition();
+				}
+
+				//Move the player to the position
+				moveToPosition();
+			}
+			else
+			{
+			}
+		}
+	}
+
+	void locatePosition()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		if(Physics.Raycast(ray, out hit, 1000))
+		{
+			if(hit.collider.tag!="Player"&&hit.collider.tag!="Enemy")
+			{
+				position = hit.point;
+			}
+		}
+	}
+
+	void locateCursor()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		
+		if(Physics.Raycast(ray, out hit, 1000))
+		{
+			cursorPosition = hit.point;
+		}
+	}
+
+	void moveToPosition()
+	{
+		//Game Object is moving
+		if(Vector3.Distance(transform.position, position)>1)
+		{
+			Quaternion newRotation = Quaternion.LookRotation(position-transform.position, Vector3.forward);
+
+			newRotation.x = 0f;
+			newRotation.z = 0f;
+
+			transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 10);
+			controller.SimpleMove(transform.forward * speed);
+
+			animation.CrossFade(run.name);
+		}
+		//Game Object is not moving
+		else
+		{
+			animation.CrossFade(idle.name);
+		}
+	}
+
+}
---
+++ LabsPlayer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LabsPlayer : MonoBehaviour {
+
+	public float moveSpeed = 5.0f;
+	public float gravity = 8.0f;
+	public GameObject player;
+	public LayerMask rayTerrain;
+	Vector3 lookPosition;
+	Vector3 destPosition;
+	Vector3 destDirection;
+	float destDistance;
+	float downforce;
+	Ray ray;
+	RaycastHit hit;
+	CharacterController controller;
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButton (0)) {
+			ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		}
+		if (Physics.Raycast (ray, out hit, Mathf.Infinity, rayTerrain)) {
+			destPosition = hit.point;
+			destDistance = Vector3.Distance(destPosition, player.transform.position);
+			player.transform.LookAt (new Vector3 (destPosition.x, player.transform.position.y, destPosition.z));
+			destDirection = destPosition - player.transform.position;
+			destDirection.y = 0;
+			destDirection = destDirection.normalized * moveSpeed;
+			destDirection.y -= gravity * 100 * Time.deltaTime;
+		}
+		if (destDistance > .2f) {
+			controller.Move (destDirection * Time.deltaTime);
+			animator.SetFloat ("Speed", 1f);
+		}
+		else {
+			animator.SetFloat ("Speed", 0f);
+		}
+	}
+}
